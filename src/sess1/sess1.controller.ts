b8552aae2ded440507362bd3f320fcfdbab040a6
Hazrat Ali <hazratali3711@gmail.com>
2024-02-08 10:19:50 +0600

    feat:initial commit

--- src/sess1/sess1.controller.ts
+++ src/sess1/sess1.controller.ts
@@ -1,10 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpCode,
+  HttpException,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+} from '@nestjs/common';
 import { Sess1Service } from './sess1.service';
 @Controller('sess1')
 export class Sess1Controller {
   constructor(private readonly sess1: Sess1Service) {}
   @Get('')
-  hello(): string {
+  hello() {
     return this.sess1.hello();
+  }
+  @Get('/:id')
+  pipe(@Param('id', ParseIntPipe) id: number) {
+    return this.sess1.pipe(id);
   }
 }
--- src/sess1/sess1.service.ts
+++ src/sess1/sess1.service.ts
@@ -1,7 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import {
+  HttpCode,
+  HttpException,
+  HttpStatus,
+  Injectable,
+} from '@nestjs/common';
 @Injectable()
 export class Sess1Service {
-  hello(): string {
-    return 'Hello from ses1 service1';
+  hello() {
+    try {
+      // await this.service.findAll();
+      console.log('Hi');
+    } catch (error) {
+      throw new HttpException(
+        {
+          status: HttpStatus.FORBIDDEN,
+          error: 'This is a custom message',
+        },
+        HttpStatus.FORBIDDEN,
+        {
+          cause: error,
+        },
+      );
+    }
+  }
+  pipe(id: number) {
+    console.log(id);
   }
 }
